4ba207b64e8cd922a2f36d5dc0ae110d839ee59f
Richard Sprague <richardsprague@gmail.com>
2021-07-08 20:38:01 -0700

    new function to write to database (doesn't completely work yet)

--- dev/psi_db_load.R
+++ dev/psi_db_load.R
@@ -18,18 +18,36 @@ Sys.setenv(R_CONFIG_ACTIVE = "local")  # save to local postgres
 # glucose_records, notes_records, sleep_and_hr
 # source("util/read_glucose_data.R")  # read raw glucose, notes, and watch data.
 
+#' @title Write a glucose dataframe to the database
+#' @description
+#' WARNING: always delete the table before running this on a `user_id` that's already in the database.
+#' (it's not finished yet and doesn't take into account previous entries)
+#' @param new_table valid formatted glucose dataframe
+psi_write_glucose <- function(conn_args = config::get("dataconnection"),
+                              user_id = 1235,
+                              new_table=glucose_df_from_libreview_csv(user_id = 1235)) {
 
-psi_write_glucose <- function(newtable=glucose_df_from_libreview_csv()) {
+    con <- DBI::dbConnect(
+        drv = conn_args$driver,
+        user = conn_args$user,
+        host = conn_args$host,
+        port = conn_args$port,
+        dbname = conn_args$dbname,
+        password = conn_args$password
+    )
+
     message("write glucose records")
-    maxDate <-
-        DBI::dbGetQuery(con, "select max(\"time\") from glucose_records;")$max
-    new_records <-
-        newtable %>% dplyr::filter(time > if_else(is.na(maxDate), min(glucose_records$time), maxDate))
+
+    DBI::dbWriteTable(con, "glucose_records", new_table, append = TRUE)
+
+    maxDate <- psiCGM:::max_date_for_user(conn_args, user_id = user_id)
+    #new_records <-
+    #    new_table %>% dplyr::filter(time > if_else(is.na(maxDate), min(conn_args$glucose_table$time), maxDate))
 
     # uncomment the following line to do the actual write to db
-    # dbWriteTable(con, name = "glucose_records", value = new_records, row.names = FALSE, append = TRUE)
+    #DBI::dbWriteTable(con, name = "glucose_records", value = new_records, row.names = FALSE, append = TRUE)
 
-    message("write notes records")
+    message("write notes records (not working yet)")
 
     # uncomment the following line
     # DBI::dbWriteTable(con, name = "notes_records", value = notes_records, row.names = FALSE, overwrite = TRUE)
