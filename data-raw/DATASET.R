a20b8f092109c48cf342cec63d77b08a7c2d73cf
Richard Sprague <richardsprague@gmail.com>
2021-07-11 07:47:56 -0700

    renamed module.  bug fix to data.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,4 @@
 ## code to prepare `sample_libreview_df` dataset goes here
 
-sample_libreview_df <- glucose_df_from_libreview_csv(system.file("extdata", "Firstname1Lastname1_glucose.csv"))
+sample_libreview_df <- glucose_df_from_libreview_csv(system.file("extdata", package = "psiCGM", "Firstname1Lastname1_glucose.csv"))
 usethis::use_data(sample_libreview_df, overwrite = TRUE)
