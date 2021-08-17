5f98f460d409930b6d96ef313579d015d6a2e280
Richard Sprague <richardsprague@gmail.com>
2021-08-17 08:22:15 -0400

    Before new branch. This version works.

--- dev/psi_deploy.R
+++ dev/psi_deploy.R
@@ -7,12 +7,21 @@ rsconnect::setAccountInfo(name=config::get("shiny")$name,
                           token=config::get("shiny")$token,
                           secret=config::get("shiny")$secret)
 
-options(repos = c("https://cran.rstudio.com",
-                  devtools::install_github("personalscience/psi-shiny-cgm")))
+# options(repos = c("https://cran.rstudio.com",
+#                   devtools::install_github("personalscience/psi-shiny-cgm")))
 
-#devtools::install_github("personalscience/psi-shiny-cgm")
+devtools::install_github("personalscience/psi-shiny-cgm")
 
 
-rsconnect::deployApp("Tastermonial")
+rsconnect::deployApp(appDir = file.path(getwd(),"R"),
+                     appName = "Tastermonial",
+                     appFiles = c("ui.R","server.R",
+                                  "read_data_utils.R",
+                                  "mod_filter.R",
+                                  "mod_calc_util.R",
+                                  "psi_user_management.R",
+                                  "config.yml"),
+                     forceUpdate = TRUE
+                     )
 
 
