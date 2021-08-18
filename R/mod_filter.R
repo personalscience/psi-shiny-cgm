c0010ddc18d08c65414f275ce7876e219cacc44c
Richard Sprague <richardsprague@gmail.com>
2021-08-18 14:10:04 -0400

    adding even more files.

---
+++ R/mod_filter.R
@@ -0,0 +1,82 @@
+# Shiny Module for filtering the Libreview glucose dataframes
+
+#' @title Shiny module (UI) to filter CGM result based on user input
+#' @param id input id for server
+#' @description
+#' Standalone UI renders HTML for a main panel
+#' @export
+mod_filterUI <- function(id) {
+
+  ns <- NS(id)
+
+    tagList(
+      selectInput(ns("user_id"), label = "User Name",
+                  choices = with(user_df_from_libreview, paste(first_name,last_name)),
+                  selected = "Martha Sprague"),
+      dateInput(ns("start_date"), label = "Start Date", value = as_datetime("2021-06-17", tz = Sys.timezone() )),
+      sliderInput(ns("start_hour"), label = "Start Time (Hour)", value = 12, min = 0, max = 23),
+      sliderInput(ns("time_length"), label = "Time length (Min)", value = 120, min = 10, max = 480, step = 30),
+      checkboxInput(ns("zoom_to_date"), label = "Zoom Day", value = FALSE),
+      # textInput(ns("zoom_to_food"), label = "Food", value = "blueberries"),
+      # actionButton(ns("submit_food"), label = "Submit Food"),
+
+
+    checkboxInput(ns("chk_sleep"), label = "Sleep", value = FALSE),
+    textOutput(ns("show_food"))
+    )
+}
+
+#' @title Shiny module (server) to filter a CGM result based on user input
+#' @param id server input id
+#' @return valid libreview-based dataframe encapsulated as a reactive
+#' @export
+mod_filterServer <- function(id){
+
+  moduleServer(id, function(input, output, session) {
+    ID<- reactive( {message(paste("Selected User", isolate(input$user_id)))
+      lookup_id_from_name(input$user_id[1])}
+      )
+    start_date <- reactive(force_tz(input$start_date,
+                                    tzone=Sys.timezone()) +
+                             lubridate::hours(input$start_hour))
+    # go_date <- reactive(if(input$submit_food) psiCGM:::food_times_df()
+    #                     else (input$start_date + lubridate::hours(input$start_time))
+    # )
+
+
+    glucose_df <- reactive({
+
+      # if(input$submit_food){ output$show_food <- renderText(paste("You made it!", input$zoom_to_food))
+      # }
+
+      if(input$zoom_to_date) {
+
+        glucose_df_from_db(user_id = ID()) %>%
+          filter(time >= start_date()) %>%
+          filter(time <= start_date() + lubridate::minutes(input$time_length))
+      } else  glucose_df_from_db(user_id = ID()) %>% filter(time >= start_date())
+    })
+    return(glucose_df)
+  })
+
+}
+
+#' @title demo a shiny UI/server combination
+#' @description useful for debugging
+demo_filter <- function(){
+
+
+  ui <- fluidPage(
+    mod_filterUI("x"),
+    dataTableOutput("table")
+  )
+
+  server <- function(input, output, session) {
+    gdf <- mod_filterServer("x")
+    output$table <- renderDataTable(gdf())
+  }
+
+  shinyApp(ui, server)
+}
+
+
---
+++ R/mod_libreviewUI.R
@@ -0,0 +1,72 @@
+# module to display plots of glucose values
+
+
+
+#' @title UI for Libreview plots
+#' @description
+#' Plot a Libreview object along with its AUC value
+#' @param id Shiny id
+#' @export
+mod_libreviewUI <- function(id) {
+  ns <- NS(id)
+
+  fluidRow(
+          plotOutput(ns("libreview")),
+          h3("AUC"),
+          textOutput(ns("auc_value")))
+
+}
+
+#' @title Make a glucose chart
+#' @description
+#' Given a (reactive) libreview dataframe, this Shiny module will
+#' generate a valid ggplot object and display it in an accompanying UI
+#' @param id shiny module id
+#' @param glucose_df reactive for a valid glucose dataframe
+#' @param title a title for the plot
+#' @return ggplot object representing a glucose chart
+#' @export
+mod_libreview_plotServer <- function(id,  glucose_df, title="Name") {
+
+  moduleServer(id, function(input, output, session) {
+    # observe({
+    #   cat(file=stderr(),
+    #       sprintf("found your dataframe with %d rows\n",nrow(glucose_df())))
+    # })
+
+    output$auc_value <- renderText(sprintf("%.2f",psiCGM::auc_calc(glucose_df())))
+
+    observeEvent(glucose_df(),
+                 {     cat(file=stderr(),
+                           sprintf("User %s dataframe still has %d rows\n",title(), nrow(glucose_df())))
+                 output$libreview <- renderPlot(plot_glucose(glucose_df(), title()))
+                 }
+    )
+
+    current_glucose <- reactive({message("inside reactive")
+                                         glucose_df()})
+
+
+    #message(current_glucose())
+    g <- reactive(plot_glucose(current_glucose(), title))
+    return(g)
+
+  })
+
+}
+
+
+#' @title Demo the libreviewUI
+cgm_demo <- function() {
+
+
+  glucose_df <- psiCGM::sample_libreview_df
+  ui <- fluidPage(mod_libreviewUI("x"))
+  server <- function(input, output, session) {
+    mod_libreview_plotServer("x", reactive(glucose_df), reactiveVal("Username"))
+  }
+  shinyApp(ui, server)
+
+}
+
+
