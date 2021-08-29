45bdb39df62fb3e466d860ee76dde69ac5c2408b
Richard Sprague <richardsprague@gmail.com>
2021-08-29 08:29:06 -0700

    staging new_food_times_df() that apparently works correctly.

--- dev/scratch.R
+++ dev/scratch.R
@@ -16,12 +16,98 @@ library(lubridate)
 #Sys.setenv(R_CONFIG_ACTIVE = "tastercloud")
 
 
+new_food_times_df <-
+  function(conn_args = config::get("dataconnection"),
+           timeLength = 120,
+           foodname = "watermelon",
+           db_filter = function(x) {
+             x
+           }) {
+    con <- DBI::dbConnect(
+      drv = conn_args$driver,
+      user = conn_args$user,
+      host = conn_args$host,
+      port = conn_args$port,
+      dbname = conn_args$dbname,
+      password = conn_args$password
+    )
 
-bind_rows(
-  food_times_df(lookup_id_from_name("Richard"),foodname = "Pop-Tart"),
-  food_times_df(lookup_id_from_name("Richard"),foodname = "Real food")
-) %>% filter(!is.na(value)) %>% group_by(meal) %>% arrange(t) %>% mutate(value = value-first(value)) %>%
-  ungroup() %>%  arrange(meal, t) %>% View()#%>% ggplot(aes(t,value, color = foodname)) + geom_line(size = 2)
+    notes_df <-
+      tbl(con, "notes_records") %>% collect() %>%  db_filter() %>%
+      filter(stringr::str_detect(
+        stringr::str_to_lower(Comment),
+        stringr::str_to_lower(foodname)
+      ))
+
+    df <- NULL
+
+    users <- unique(notes_df$user_id)
+
+    for (user in users) {
+      f <- tbl(con, "glucose_records") %>% filter(user_id == user) %>% filter(!is.na(value))
+      times <- notes_df %>% filter(user_id == user)  %>% pull(Start)
+      for (atime in times) {
+
+        t0 <- as_datetime(atime)
+        tl <- as_datetime(t0 + minutes(timeLength))
+
+        new_df <- f %>%
+          filter(time >= t0 & time <= tl) %>% collect() %>%
+          transmute(t = as.numeric(time - min(time))/60,
+                    value = value,
+                    meal=sprintf("%s%s-%i/%i",
+                            substring(username_for_id(user),1,1),
+                            str_split(username_for_id(user),
+                                      " ")[[1]][2],
+                            month(as_datetime(atime)),
+                            day(as_datetime(atime))),
+               foodname = sprintf("%s-%i/%i",
+                                  foodname,
+                                  month(as_datetime(atime)),
+                                  day(as_datetime(atime))),
+               user_id = factor(user_id)) #user_id=factor(user_id, levels = original_levels))
+        #
+
+        # df <- bind_rows(df, transmute(new_df,
+        #                               t=zero_time(time),
+        #                               value=value,
+        #                               meal=meal,
+        #                               foodname = foodname,
+        #                               user_id=user_id))
+
+       df <- bind_rows(df,new_df)
+
+      }
+    }
+
+  return(df)
+
+}
 
 
+# new_food_times_df(foodname = "blueberries") %>% distinct(user_id)
+# food_times_df(foodname="blueberries") %>% distinct(user_id)
 
+notes_df <-
+  tbl(con, "notes_records") %>% collect()  %>%
+  filter(stringr::str_detect(
+    stringr::str_to_lower(Comment),
+    stringr::str_to_lower("blueberries")
+  ))
+
+
+system.time(new_food_times_df(foodname = "blueberries"))
+system.time(food_times_df(foodname="blueberries"))
+
+n <- new_food_times_df(foodname = "blueberries") #%>% group_by(meal) %>% arrange(t) %>% summarize(t, value)
+  # group_by(meal) %>% arrange(time) %>%
+  # transmute(t=time - first(time),
+  #           value=value,
+  #           meal=meal,
+  #           foodname = foodname,
+  #           user_id=user_id) %>% ungroup()
+
+
+x <- food_times_df(foodname="blueberries")
+
+#new_food_times_df(foodname = "blueberries") %>% arrange(time)# %>% group_by(meal) %>% arrange(time)
